b7d683cb1f9af28dc2df9ba643063652dd1ec899
Fredrik Håård <fredrik.haard@wrapp.com>
2018-04-26 09:16:44 +0200

    Initial copy from transactions

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kinesis Producers
+
+Library for posting messages to Kinesis Firehose and Streams (planned). Includes basic re-try, fallback and buffering functionality.
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="kinesis-producer",
+    version="0.0.1a1",
+    packages=['wrapp', 'wrapp.kinesisproducer'],
+    author="Fredrik Håård",
+    author_email="fredrik@metallapan.se",
+    description="Single client for producing Kinesis Firehose or Stream (planned) records. Python 3.3+.",
+    license="MIT",
+    keywords="aws kinesis firehose streams",
+    url="https://github.com/wrapp/kinesisproducer",
+    classifiers="""Development Status :: 3 - Alpha
+Intended Audience :: Developers
+Topic :: Software Development :: Libraries
+License :: Freely Distributable
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5""".split('\n'),
+    long_description=open('README.md', 'rt').read(),
+)
